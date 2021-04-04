358cee7efd3ebaa5a1ed02186090a452ce29ed8d
parneet2004 <parneetsidhu2004@gmail.com>
2021-04-04 10:58:50 +0530

    Some stuff

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def homepage(request):
-    # request -> POST,GET
-    # location of file
-    # templates/index.html
+    print(request.POST.get('username'))
     return render(request, 'index.html', context={"name": "Parneet"})
