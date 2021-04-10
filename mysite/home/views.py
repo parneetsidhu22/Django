b66ca6cb2dfef0a75d80dd5836d3d5f436653d03
aman-gill22 <gillamanindersingh@gmail.com>
2021-04-10 10:18:38 +0530

    saturday work

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -3,5 +3,10 @@ from .models import Blog
 
 
 def homepage(request):
+    if request.method == "POST":
+        msg=request.POST.get("msg_box")
+        if msg!='':
+            blog=Blog(msg=msg)
+            blog.save()
 
     return render(request, 'index.html', context={"name": "Parneet"})
